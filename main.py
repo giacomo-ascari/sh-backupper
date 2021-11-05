9353fdddb474c4b02770e40b8d10e126b114047f
giacomo-ascari <giacomolego@gmail.com>
2021-11-05 12:00:32 +0100

    feat: stuff

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import os
 from dotenv import load_dotenv
 from utils import log
+import time
+import datetime
 import json
 import requests
 load_dotenv()
@@ -17,8 +19,10 @@ def handle_http(item):
     }
     return data
 
-def save_data(data, item):
-    with open(item["desc"]+".json", "w") as f:
+def save_data(data, item, date):
+    if not os.path.exists(date):
+        os.mkdir(date)
+    with open(date + "/" + item["desc"]+".json", "w") as f:
         json.dump(data, f, indent=4)
 
 supported_types = {
@@ -40,7 +44,10 @@ for i in data:
 def main():
     try:
 
-        log("main", "Started. Loading hosts.json...")
+        d = datetime.datetime.now()
+        date = "{:%Y%m%d-%H%M%S}".format(d)
+        log("main", "Started: "+ date)
+        log("main", "Loading hosts.json...")
         with open("hosts.json") as f:
             items = json.load(f)
         log("main", "Hosts.json loaded")
@@ -53,15 +60,29 @@ def main():
                 raise Exception("Wrong preliminary fields' type")
             
             if item["type"] in supported_types.keys():
-                log(item["desc"], "Supported. Now starting...")
-                data = supported_types[item["type"]](item)
-                save_data(data, item)
+                try:
+                    log(item["desc"], "Supported. Now starting...")
+                    data = supported_types[item["type"]](item)
+                    save_data(data, item, date)
+                    log(item["desc"], "Saved")
+                except:
+                    log(item["desc"], "ERROR. Not saved")
             else:
-                log(item["desc"], "Not supported")
-                raise Exception("Unsopported type")
+                log(item["desc"], "ERROR. Not supported")
+        try:
+            if os.path.exists(date):
+                log("sftp", "Establishing connection...")
+                srv = pysftp.Connection(host=os.getenv("SFTP_IP"), username=os.getenv("SFTP_USER"), password=os.getenv("SFTP_PW"))
+                data = srv.listdir()
+                srv.close()
+                for i in data:
+                    print(i)
+        except:
+            log("sftp", "ERROR")
+
 
     except Exception as e:
-        log("error", e)
+        log("error", "Unhandled error", e)
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,9 +1,19 @@
 def colored(r, g, b, text):
     return "\033[38;2;{};{};{}m{}\033[39m".format(r, g, b, text)
 
+def heighten(a, b, c):
+    if a > max(b, c):
+        a = 255
+    elif b > max(a, c):
+        b = 255
+    else:
+        c = 255
+    return a, b, c
+
 def log(head, body, obj=None):
     h = hash(head)
     r, g, b = int(h%256), int(h/10**3%256), int(h/10**6%256)
+    r, g, b = heighten(r, g, b)
     head_len = len(head)+2
     print(colored(r, g, b, "[" + head.upper() + "]"), body)
     if obj:
